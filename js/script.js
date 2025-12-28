a23eedd295b062bdac1f7f372f52b085b8edb64e
massimilianomiceli <miceli.massimiliano@geoconsultsrl.it>
2025-12-28 10:42:59 +0100

    aggiunto modifica dell alt durante il cambio delle immagini

--- js/script.js
+++ js/script.js
@@ -6,10 +6,12 @@ var turnOn = false;
 btnLamp.addEventListener("click", function(){
     if (turnOn) {
         imgLamp.src = './img/white_lamp.png';
+        imgLamp.alt = 'white lamp';
         btnLamp.textContent = 'Accendi';
         turnOn = false;
     } else {
         imgLamp.src = './img/yellow_lamp.png';
+        imgLamp.alt = 'yellow lamp';
         btnLamp.textContent = 'Spegni';
         turnOn = true;
     }
