7a5e6ad05a66bb5a2b6d7e52798fef9474c875f1
massimilianomiceli <miceli.massimiliano@geoconsultsrl.it>
2025-12-28 10:21:57 +0100

    aggiunto collegamento allo script ed assegnate le variabili immagine e pulsante

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const imgLamp = document.getElementById("img-lamp");
+const btnLamp = document.getElementById("btn-lamp");
+
+onsole.log(btnLamp);
+
+btnLamp.addEventListener("click", function(){
+    console.log("Ciao");
+});
